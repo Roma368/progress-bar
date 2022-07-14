573b493ce48e495a24db7eb59da3d99e132810bd
Roma368 <guest.n345@gmail.com>
2022-07-14 19:15:52 +0300

    second commit

--- scripts/main.js
+++ scripts/main.js
@@ -6,15 +6,14 @@
 
 let inputValue = document.getElementById('myRange').value
 let element = document.querySelector('#circle').style.background
-let backgroundValue = 'conic-gradient(#922c8a 0 ' + inputValue + '%, #27282b ' +inputValue + '% 100%);'
-console.log(element)
-console.log(backgroundValue)
+let getBackgroundValue = document.getElementById('circle').style.background
+console.log(document.getElementById('circle').style.background)
 
 function getValue (){
     inputValue = document.getElementById('myRange').value
     document.getElementById('circle-text').innerHTML = inputValue
-    backgroundValue = 'conic-gradient(#922c8a 0 ' + inputValue + '%, #27282b ' +inputValue + '% 100%);'
-    document.getElementById('circle').style.background = backgroundValue
+    getBackgroundValue = 'conic-gradient(rgb(146, 44, 138) 0deg, rgb(146, 44, 138) ' + inputValue + '%, rgb(39, 40, 43) ' + inputValue + '%, rgb(39, 40, 43) 100%);'
+    document.getElementById('circle').style.background = getBackgroundValue
     console.log(document.getElementById('circle').style.background)
 }
 
