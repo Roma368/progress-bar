82b420c7023035e9121b421ff47717b0eb2f66cc
Roma368 <guest.n345@gmail.com>
2022-07-16 01:58:28 +0300

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,11 @@
-// let valueOfRange = document.getElementById('myRange').value
-// let element = document.querySelector('#circle')
-// let computedStyle = getComputedStyle(element)
-// let backgroundStyledOfCircle = computedStyle.backgroundImage
-// console.log(backgroundStyledOfCircle)
+
 
 let inputValue = document.getElementById('myRange').value
-let element = document.querySelector('#circle').style.background
-let getBackgroundValue = document.getElementById('circle').style.background
-console.log(document.getElementById('circle').style.background)
 
 function getValue (){
     inputValue = document.getElementById('myRange').value
     document.getElementById('circle-text').innerHTML = inputValue
-    getBackgroundValue = 'conic-gradient(rgb(146, 44, 138) 0deg, rgb(146, 44, 138) ' + inputValue + '%, rgb(39, 40, 43) ' + inputValue + '%, rgb(39, 40, 43) 100%);'
-    document.getElementById('circle').style.background = getBackgroundValue
-    console.log(document.getElementById('circle').style.background)
+    document.getElementById('circle').style.background = 'conic-gradient(#922c8a 0 '+inputValue+'%, #27282b '+inputValue+'% 100%)'
 }
 
 $('.slider').mousemove(function () {
